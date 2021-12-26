4bbccb5ee43b450823ee3461d21c8fccf024527f
christian-cell <Java_s_ninja69>
2021-12-26 20:01:39 +0100

    añadiendo insert en el index.php

--- website/connection.php
+++ website/connection.php
@@ -1,13 +1,41 @@
 <?php
 
-echo "reconoce este archivo";
+/* POO */
 
-$host = $_ENV["MYSQL_SERVICE_SERVICE_HOST"];
+namespace smartCalendar;
+include_once('cors.php');
+
+class Conexion {
+
+  protected $link;
+
+  
+
+  function Connect()
+  {
+    $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+    $host = $actual_link === "http://localhost:8080/" ? "db" : $_ENV["MYSQL_SERVICE_SERVICE_HOST"];
+    
+    
+    $this->link = mysqli_connect($host , "root" , "secret" , "usuarios");
+    return $this->link;
+  }
+}
+
+$conexion_result = new Conexion();
+$conexion_result->Connect();
+
+
+// var_dump($actual_link);
+
+/* echo "reconoce este archivo";
+
+$host = $_ENV["MYSQL_SERVICE_SERVICE_HOST"]; */
 /* $user = $_ENV["MYSQL_USER"];
 $pass = $_ENV["MYSQL_PASSWORD"];
 $db = $_ENV["MYSQL_DATABASE"]; */
 
-$link = mysqli_connect($host,"root","secret","usuarios");
+/* $link = mysqli_connect($host,"root","secret","usuarios");
 
 $SQL = "INSERT INTO clientes
  (
@@ -22,6 +50,6 @@ $SQL = "INSERT INTO clientes
 ";
 
 $resp = mysqli_query($link , $SQL)or die(mysqli_error($link));
-var_dump($resp); 
+var_dump($resp); */ 
 
 ?>
--- website/index.php
+++ website/index.php
@@ -1,6 +1,35 @@
 <?php
 
 
-echo "este es el index.php hello world buddy"
+namespace smartCalendar;
+include_once('connection.php');
+include_once('cors.php');
+
+class NuevoCliente extends Conexion{
+
+  function CreateClient  ()
+  {
+    $link = $this->Connect();
+
+    $SQL = "INSERT INTO clientes
+        (
+            nombre,
+            apellidos,
+            dni
+        )VALUES(
+            'carlos',
+            'garcia , romero',
+            '83892902'
+        )
+    ";
+
+    $resp = mysqli_query($link , $SQL) or die(mysqli_error($link));
+    echo $resp;
+  }
+
+}
+
+$nuevoClienteClass = new NuevoCliente();
+$nuevoClienteClass->CreateClient();
 
 ?>
