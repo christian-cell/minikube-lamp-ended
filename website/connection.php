243ac51f46173334cb6578ee1b12aaa5e0db912d
christian-cell <Java_s_ninja69>
2021-12-26 17:52:50 +0100

    fixing nginx-ingress giving default path for phpmyadmin an /server for apachephp

--- website/connection.php
+++ website/connection.php
@@ -2,7 +2,12 @@
 
 echo "reconoce este archivo";
 
-/* $link = mysqli_connect("db","root","secret","usuarios");
+$host = $_ENV["MYSQL_SERVICE_SERVICE_HOST"];
+/* $user = $_ENV["MYSQL_USER"];
+$pass = $_ENV["MYSQL_PASSWORD"];
+$db = $_ENV["MYSQL_DATABASE"]; */
+
+$link = mysqli_connect($host,"root","secret","usuarios");
 
 $SQL = "INSERT INTO clientes
  (
@@ -17,6 +22,6 @@ $SQL = "INSERT INTO clientes
 ";
 
 $resp = mysqli_query($link , $SQL)or die(mysqli_error($link));
-var_dump($resp); */
+var_dump($resp); 
 
 ?>
